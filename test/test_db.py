11928bce9be456d3e133a827d35686fa74ea8a92
ndrini <aless@ndrini.eu>
2021-02-06 16:00:03 +0100

    format files

--- test/test_db.py
+++ test/test_db.py
@@ -4,42 +4,38 @@ import csv
 
 from usefull import guess_type
 
-def read_db() -> list:
+
+def read_db(file_name) -> list:
     result = []
-    try: 
-        with open( os.path.dirname(__file__) + '/../db_simple.csv') as f:
+    try:
+        with open(os.path.dirname(__file__) + '/../' + file_name) as f:
             reader = csv.DictReader(f)
-            
-              '''
-            {'page': '1', 'msg': '1. Mi sembra che', 'parent': '0', 'choice': 'False', 'end': 'False'}
-            {'page': '2', 'msg': '...se ti trovassi', 'parent': '1', 'choice': 'True', 'end': 'False'}
-            '''
             for line in reader:
                 print("Not typed data\t: ", line)
-                for key in line: 
-                    line[key] = guess_type(line[key])    
-                result.append(line)    
+                for key in line:
+                    line[key] = guess_type(line[key])
+                result.append(line)
                 print("Typed data\t: ", line)
 
     except OSError:
         print("\t*** No database file!")
 
-    return result 
-
-def test_read_csv_db():
-
-    assert read_db()[0]['page'] == 1
-    
-
-''' 
-[{'page': 1, 'msg': "1. Mi sembra che", 'parent': 0, 
-  'level': 'S1', 'label': 1, 'choice': False, 'end': False}, 
- {'page': 2, 'msg': '...se ti trovassi', 'parent': 1, 
-  'level': 'S1_2', 'label': 'A', 'choice': True, 'end': False}, ... ]
+    return result
 
 
-page	msg	parent	choice	end
-1	1. Mi sembra che	0	False	False
-2	...se ti trovassi	1	True	False
+def test_read_csv_db_simple():
+    '''
+    [{'page': 1, 'msg': "1. Mi sembra che", 'parent': 0,
+    'level': 'S1', 'label': 1, 'choice': False, 'end': False},
+    {'page': 2, 'msg': '...se ti trovassi', 'parent': 1,
+    'level': 'S1_2', 'label': 'A', 'choice': True, 'end': False}, ... ]
 
-'''
+    page	msg	parent	choice	end
+    1	1. Mi sembra che	0	False	False
+    2	...se ti trovassi	1	True	False
+    '''
+    assert read_db('db_simple.csv')[0]['page'] == 1
+
+
+def test_read_real_db():
+    assert len(read_db('db.csv')) == 167
--- usefull.py
+++ usefull.py
@@ -1,19 +1,19 @@
 import os
 
+
 def custom_path(script_dir, file_name) -> str:
     parent_dir = os.path.split(script_dir)[0]
     file_path = os.path.join(parent_dir, file_name)
-    return  file_path
+    return file_path
+
 
 def guess_type(x):
-    # credits           
+    # credits
     # https://stackoverflow.com/questions/19080007/
     # csv-reader-and-dictreader-turn-numeric-fields-into-strings
     attempt_fns = [int,
-                   bool, 
+                   bool,
                    float,
-                   lambda x: datetime.datetime.strptime(x, 
-                                                    "%Y-%m-%d %H:%M:%S")
                    ]
     for fn in attempt_fns:
         try:
