b6753a7fd49cb4eab1d120405def07c460ed049e
ndrini <aless@ndrini.eu>
2021-02-06 15:36:25 +0100

    open the csv file, with the right value types

--- test/test_db.py
+++ test/test_db.py
@@ -1,21 +1,45 @@
 import main
 import os
+import csv
 
-from usefull import custom_path
+from usefull import guess_type
 
 def read_db() -> list:
-    script_dir = os.path.dirname(__file__)
+    result = []
     try: 
-        with open(custom_path(script_dir,'db.csv')) as f:
-            print("****************** ok, found with custom path!")     
-
+        with open( os.path.dirname(__file__) + '/../db_simple.csv') as f:
+            reader = csv.DictReader(f)
+            
+              '''
+            {'page': '1', 'msg': '1. Mi sembra che', 'parent': '0', 'choice': 'False', 'end': 'False'}
+            {'page': '2', 'msg': '...se ti trovassi', 'parent': '1', 'choice': 'True', 'end': 'False'}
+            '''
+            for line in reader:
+                print("Not typed data\t: ", line)
+                for key in line: 
+                    line[key] = guess_type(line[key])    
+                result.append(line)    
+                print("Typed data\t: ", line)
 
     except OSError:
-        print("******************No database file!")
+        print("\t*** No database file!")
 
-    return [{'page': 1, 'parent': 0}] 
+    return result 
 
 def test_read_csv_db():
 
     assert read_db()[0]['page'] == 1
     
+
+''' 
+[{'page': 1, 'msg': "1. Mi sembra che", 'parent': 0, 
+  'level': 'S1', 'label': 1, 'choice': False, 'end': False}, 
+ {'page': 2, 'msg': '...se ti trovassi', 'parent': 1, 
+  'level': 'S1_2', 'label': 'A', 'choice': True, 'end': False}, ... ]
+
+
+page	msg	parent	choice	end
+1	1. Mi sembra che	0	False	False
+2	...se ti trovassi	1	True	False
+
+'''
--- usefull.py
+++ usefull.py
@@ -4,3 +4,20 @@ def custom_path(script_dir, file_name) -> str:
     parent_dir = os.path.split(script_dir)[0]
     file_path = os.path.join(parent_dir, file_name)
     return  file_path
+
+def guess_type(x):
+    # credits           
+    # https://stackoverflow.com/questions/19080007/
+    # csv-reader-and-dictreader-turn-numeric-fields-into-strings
+    attempt_fns = [int,
+                   bool, 
+                   float,
+                   lambda x: datetime.datetime.strptime(x, 
+                                                    "%Y-%m-%d %H:%M:%S")
+                   ]
+    for fn in attempt_fns:
+        try:
+            return fn(x)
+        except (ValueError, SyntaxError):
+            pass
+    return x
